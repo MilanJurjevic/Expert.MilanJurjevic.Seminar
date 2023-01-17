c3828b4cf00588a4fcab52c4d263a47772e6feb6
milan <milan@DESKTOP-MM9QA3A>
2023-01-17 20:16:38 +0100

    Add project files.

---
+++ Expert.MilanJurjevic.Seminar/Controllers/SeminarController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace Expert.MilanJurjevic.Seminar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeminarController : ControllerBase
+    {
+        [HttpGet("Ime/{Ime}/Prezime/{Prezime}")]
+        public ActionResult<string> VratiIme(string Ime, string Prezime)
+        {
+            string Name = Ime;
+            string Surname = Prezime;
+            string Opis = "Saminar napravio: ";
+            return (Opis + Ime + " " + Prezime);
+
+        }
+
+        [HttpGet("prvibroj/{prviBroj}/drugibroj/{drugiBroj}")]
+
+        public ActionResult<int> Zbroji(int prviBroj, int drugiBroj)
+        {
+            int prvi = prviBroj;
+            int drugi = drugiBroj;
+            int zbroj = prvi + drugi;
+            return (zbroj);
+
+
+        }
+
+        [HttpGet("prvibr/{prviB}/drugiB/{drugiB}")]
+
+        public ActionResult<string> Zbroji1(int prviB, int drugiB)
+        {
+            int prvi = prviB;
+            int drugi = drugiB;
+            int zbroj = prviB + drugiB;
+            string Zbroj = Convert.ToString(zbroj);
+            return ("Rezutlat dva prosljeđena broja je " + Zbroj);
+
+        }
+
+        [HttpGet("prvibr/{prviB}/drugibr/{drugiB}")]
+        public ActionResult<int> Vrati1(int prviB, int drugiB)
+        {
+            int prvi = prviB;
+            int drugi = drugiB;
+
+            if (prvi > drugi)
+            {
+                return (prvi);
+            }
+            else
+            {
+                return (drugi);
+            }
+        }
+
+        [HttpGet("prvi/{prviBroj}/drugi/{drugiBroj}")]
+
+        public ActionResult<int> Vrati(string prviBroj, string drugiBroj)
+        {
+            int prvi = Convert.ToInt32(prviBroj);
+            int drugi = Convert.ToInt32(drugiBroj);
+
+            if (prvi < drugi)
+
+            {
+                return (prvi + drugi);
+
+            } else if (prvi == drugi)
+            {
+                return (prvi * drugi);
+            } else
+            {
+                return (prvi - drugi);
+
+            }
+
+        }
+
+        [HttpGet("prvi/{prviBroj}")]
+        public ActionResult<List<string>> Vrati3(int prviBroj)
+        {
+            List<string> zapetljanaLista = new List<string>();
+            int rez;
+            for (int i = 1; i <= 10; i++)
+            { string brojac = Convert.ToString(i);
+                rez = i * prviBroj;
+                string zbroj = Convert.ToString(rez);
+                zapetljanaLista.Add("rezultat " + brojac+ "-te iteracije je: " + zbroj);
+             
+            }
+            return zapetljanaLista;
+
+
+        }
+    }
+}
